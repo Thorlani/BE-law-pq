ca689176b4f54cf0a6545d53e4d657ab98825bd7
Omotolani Popoola <omotolanipopoola@DotPays-MacBook-Pro-2.local>
2022-08-28 20:14:00 +0100

    wip

--- routes/getConstitutional.js
+++ routes/getConstitutional.js
@@ -10,4 +10,13 @@ router.get('/', async(req, res) => {
     }
 })
 
+router.get('/:postId', async (req, res) => {
+    try {
+        const posts = await Contract.findById(req.params.postId)
+        res.json(posts)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 module.exports = router
